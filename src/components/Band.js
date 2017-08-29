6d578fdee659dd4b070e8247f3d8a483bfb34437
Diljeet Singh <jeetds1994@gmail.com>
2017-08-29 17:10:25 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  delete = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id
+    })
+  }
   render() {
     return(
       <div>
-        band component
+        <li id={this.props.id}>{this.props.band.text}</li>
+        <button onClick={this.delete}></button>
       </div>
     );
   }
